e15a939acaf34eed9eb0decf46934ef18a35d2cf
UsamDaw <148767954+UsamDaw@users.noreply.github.com>
2024-06-05 21:41:40 +0200

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// Get the link and dropdown content elements
 var kontaktLink = document.getElementById("kontaktLink");
 var dropdownContent = document.getElementById("dropdownContent");
 
-// Add click event listener to the link
 kontaktLink.addEventListener("click", function() {
-    // Toggle the active class to show/hide dropdown content
     dropdownContent.classList.toggle("active");
 });
